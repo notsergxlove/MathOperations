e8a67a0bb08ae5681b994f70cdff6a8786a74536
notsergxlove <notsergxlove@gmail.com>
2025-09-30 00:41:45 +0600

    Add files via upload

---
+++ TestGitHubActions/MathOperations.cs
@@ -0,0 +1,26 @@
+﻿namespace TestGitHubActions
+{
+    public class MathOperations
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Mult(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+
+}
---
+++ TestGitHubActions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestGitHubActions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
